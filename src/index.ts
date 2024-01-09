7c1b7bd95a902cc19b943c167b7f44adbe5d5bac
Danil <danil.nizamutdinov7@mail.ru>
2024-01-09 20:54:20 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+alert("hello world");
+
+const getSum = (num1: number, num2: number):number => {
+    return num1 + num2
+}
+
+alert(getSum(1,2))
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3002,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: "/node_modules",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+};
